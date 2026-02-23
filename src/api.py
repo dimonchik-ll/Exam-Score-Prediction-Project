8d6766f2373a586cc68d89d622fc35addc9034e9
dimonchik-ll <diminion100@gmail.com>
2026-02-23 18:22:15 +0300

    Add fastapi backend

---
+++ src/api.py
@@ -0,0 +1,41 @@
+import joblib
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class StudentData(BaseModel):
+    age: int
+    gender: str
+    course: str
+    study_hours: float
+    class_attendance: float
+    internet_access: str	
+    sleep_hours: float
+    sleep_quality: str
+    study_method: str
+    facility_rating: str
+    exam_difficulty: str
+
+app = FastAPI()
+
+model = joblib.load("models_and_pipelines/final_model.pkl")
+
+@app.post("/score")
+def score(data: StudentData):
+    X = pd.DataFrame([{
+        "age": data.age,
+        "gender": data.gender,
+        "course": data.course,
+        "study_hours": data.study_hours,
+        "class_attendance": data.class_attendance,
+        "internet_access": data.internet_access,
+        "sleep_hours": data.sleep_hours,
+        "sleep_quality": data.sleep_quality,
+        "study_method": data.study_method,
+        "facility_rating": data.facility_rating,
+        "exam_difficulty": data.exam_difficulty
+    }])
+
+    prediction = model.predict(X)[0]
+
+    return {"score": prediction}
